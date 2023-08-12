017674cbd6d5710338cd4cece7c552f55967ded5
Davikky <ayobamivikky@gmail.com>
2023-08-11 19:13:27 -0700

    4. Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,19 +2,24 @@
 
 import hidden_4
 
+
 def uncover():
     """My function to uncover hidden_4 module
+
 
     Args:
         N/A
 
+
     Returns:
         No return value.
     """
+
     name = dir(hidden_4)
     for i in name:
         if i[:2] != '__':
             print("{:s}".format(i))
+
 
 if __name__ == "__main__":
     uncover()
